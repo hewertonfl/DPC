277daf40bf924757e9547ea4c791e1bc5319d898
Hewerton <hewerton.fl@gmail.com>
2022-11-01 20:36:30 -0300

    Update | Style file import correction

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
-app = dash.Dash(__name__,external_stylesheets=[dbc.themes.SLATE,"assets/estilo.css"],suppress_callback_exceptions=True, use_pages=True,meta_tags=[{'name': 'viewport',
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.SLATE,"assets/style.css"],suppress_callback_exceptions=True, use_pages=True,meta_tags=[{'name': 'viewport',
                 'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
 
 app.css.config.serve_locally = True,
